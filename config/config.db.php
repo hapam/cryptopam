39f0bc9da44ee6dd05ca9bc8ce2bb0f571282e7f
Hà Pam <lymanhha@gmail.com>
2017-07-28 16:50:39 +0700

    thay doi cau hinh DB

--- config/config.db.php
+++ config/config.db.php
@@ -1,9 +1,9 @@
 <?php
 // Master server DB
-define('DB_MASTER_SERVER','localhost'); //db host
-define('DB_MASTER_USER','root'); // user
-define('DB_MASTER_PASSWORD',''); // password
-define('DB_MASTER_NAME','crypto'); // db name
+define('DB_MASTER_SERVER','lgg2gx1ha7yp2w0k.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'); //db host
+define('DB_MASTER_USER','ode63qk8k9s6bb1u'); // user
+define('DB_MASTER_PASSWORD','gr8psfiukgxyc96k'); // password
+define('DB_MASTER_NAME','t13r5yr1fwzdzwq9'); // db name
 define('DB_CHARSET','UTF8'); //Set default charset DB : LATIN / UTF8
 define('EMAIL_NOTIFY_DB', 'lymanhha@gmail.com'); // When has sql error, an email will be sent to this email
 
